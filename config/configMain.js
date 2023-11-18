bb7702265af8b26e7f87aeb9518ba493257882bc
JeganTechCode <150325026+JeganTechCode@users.noreply.github.com>
2023-11-18 07:08:11 -0500

    Add files via upload

---
+++ config/configMain.js
@@ -0,0 +1,19 @@
+module.exports = {
+    ADVANCED_SEARCH_URL: 'https://ott-details.p.rapidapi.com/advancedsearch',
+    GET_PLATFORMS_URL: 'https://ott-details.p.rapidapi.com/getPlatforms',
+    X_RAPIDAPI_Key: '84b415e070msh7cf4b5bcba2c2efp1ac9a1jsn196e97375960',
+    X_RAPIDAPI_Host: 'ott-details.p.rapidapi.com',
+    HTTP_GET_METHOD: 'GET',
+    // LIST_OF_REGION: 'IN',
+    START_YEAR: '1970',
+    END_YEAR: '2020',
+    MIN_IMDB: '6',
+    MAX_IMDB: '7.8',
+    GENRE: 'action',
+    LANGUAGE: 'english',
+    TYPE: 'movie',
+    SORT: 'latest',
+    PAGE: '1'
+}
+// c808481c6bmsh4f2aede2f13ce54p106c0djsn20ce3398f361
+//84b415e070msh7cf4b5bcba2c2efp1ac9a1jsn196e97375960
